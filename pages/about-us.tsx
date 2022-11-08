d73f03c25bb365053a5659566876049f12515401
augustwoc <93571814+augustwoc@users.noreply.github.com>
2022-11-08 17:15:50 +0100

    Update about-us.tsx

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -5,7 +5,29 @@ const Aboutus = () => {
   return (
     <div>
       <Navbar />
-      <div></div>
+      <div>
+           <Head>
+        <title>Wuertz Media</title>
+        <meta
+          name="description"
+          content="Sit back, relax, and watch the numbers roll in."
+        />
+        <link rel="icon" href="/favicond.ico" />
+      </Head>
+      <Navbar />
+      <div>
+        <section className="items-center sm:mr-32 sm:ml-32 mt-32 flex justify-center xl:text-left text-center">
+          <div>
+            <h1 className="font-bold sm:text-5xl sm:max-w-2xl text-3xl">
+              Reach <span className="text-rose-500">exponential growth</span>,
+              without needing to touch your ads manager.
+            </h1>
+            <p className="mt-6 max-w-2xl font-medium text-slate-100">
+              We can manage everything for you, from creatives, to social media
+              and advertisement management. Just sit back, relax and watch the
+              numbers roll in.
+            </p>
+      </div>
       <Footer />
     </div>
   );
