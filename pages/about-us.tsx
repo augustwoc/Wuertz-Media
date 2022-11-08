b195e54b4421a6d3cf15ebc22f011703a0a2441a
augustwoc <93571814+augustwoc@users.noreply.github.com>
2022-11-08 18:50:41 +0100

    Update about-us.tsx

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -1,6 +1,6 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import Head from "../next/head";
+import Head from "next/head";
 
 const Aboutus = () => {
   return (
