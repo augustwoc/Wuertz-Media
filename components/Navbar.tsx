45a37e56da377c570ee05a4ebdbd6946b22e85c0
augustwoc <93571814+augustwoc@users.noreply.github.com>
2022-11-08 17:45:44 +0100

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className="flex justify-between items-center w-full fixed z-[1] top-0 shadow-lg p-3">
       <Link href="/">
         <h1 className="font-bold sm:text-xl text-sm cursor-pointer">
-          EXPONENTIAL STUDIOS
+          WUERTZ MEDIA
         </h1>
       </Link>
       <Link href="/about-us">
@@ -13,7 +13,7 @@ const Navbar = () => {
           About us
         </p>
       </Link>
-      <a href="https://calendly.com/exponentialstudios/initial-call">
+      <a href="https://calendly.com/wuertzmedia/60-minute-meeting?back=1&month=2022-11">
         <div className="sm:text-lg text-md bg-white pr-8 pl-8 pt-2 pb-2 rounded-md text-black font-medium flex items-cente hover:bg-slate-100 transition">
           <p>Book a Call</p>
           <div className="ml-2">
