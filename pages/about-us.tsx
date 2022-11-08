47a2e1fe2704c28f6c5aef2a2b1c16f5f6897c1a
augustwoc <93571814+augustwoc@users.noreply.github.com>
2022-11-08 16:40:24 +0100

    Create about-us.tsx

---
+++ pages/about-us.tsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const Aboutus = () => {
+  return (
+    <div>
+      <Navbar />
+      <div></div>
+      <Footer relative={false} />
+    </div>
+  );
+};
+
+export default Aboutus;
